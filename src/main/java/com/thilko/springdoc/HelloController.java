4409b66a48aa6c2de321647c39d5585172d85bf8
thilko <thilko.richter@googlemail.com>
2014-02-17 21:27:14 +0100

    removed mvc from tests since it is not needed

--- src/main/java/com/thilko/springdoc/HelloController.java
+++ src/main/java/com/thilko/springdoc/HelloController.java
@@ -1,9 +1,12 @@
 package com.thilko.springdoc;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,4 +16,17 @@ public class HelloController {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Test> test(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return new ResponseEntity<Test>(new Test(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView test1(ModelMap model) {
+        return null;
+    }
+
+
 }
